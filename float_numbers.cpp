fd1c8cb7555ab74705ec5a9dd143cadb385c7200
lailatarp122-svg <lailatarp122@gmail.com>
2026-05-14 13:55:29 +0200

    Added C++ assigment

---
+++ float_numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+float add(float a,float b,float c,float d){
+    return a+b+c+d;
+}
+
+int main(){
+    cout<<"Laila Result = "<<add(1.1,2.2,3.3,4.4);
+}
